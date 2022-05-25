c6895ed248cfc35f3fc972b47e82de9bda745f25
kaudo <kaudo@msn.com>
2022-05-25 11:43:39 +0900

    PRO21

--- js/default.js
+++ js/default.js
@@ -1,16 +1,11 @@
+$(function(){
 
-window.onload = () => {
-	console.clear();
-	var controller = new ScrollMagic.Controller();
-	
-	// build scene
-	var pageDuration = 1300;
-	
-	for ( var i = 1; i <= 5; i++ ) {
-		var scene = new ScrollMagic.Scene({triggerElement: ".pages .page-" + i, duration: pageDuration, triggerHook:0})
-		.setPin(".pages .page-" + i)
-		.addIndicators({name: i + " (duration: " + pageDuration + ")"})
-		.addTo(controller);
-	}
+	$('#btn12Welve').click(function(){
+	});
 
-}
+	$('#btnConnect').click(function(){
+	  alert('Coming soon');
+	});
+
+
+});
